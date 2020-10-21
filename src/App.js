1f168ea665917531bd16d09e665ec30b09f4b506
Peter Murphy <peterkmurphy@gmail.com>
2020-10-21 23:01:44 +1000

    Unmasked carousel

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Carousel from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    const testFolder = "/unmasked/";
+    const unmaskedFiles = [
+      "IMG_0049.jpg",
+      "IMG_0305.jpg",
+      "IMG_0365.jpg",
+      "IMG_2466.jpg",
+      "Megan 2.jpg",
+      "IMG_0120.jpg",
+      "IMG_0321.jpg",
+      "IMG_0380.jpg",
+      "IMG_9323.jpg",
+      "Megan.jpg",
+      "IMG_0142.jpg",
+      "IMG_0339.jpg",
+      "IMG_0400.jpg",
+      "IMG_9751.JPG",
+      "Paul copy.jpg",
+      "IMG_0155.jpg",
+      "IMG_0341.jpg",
+      "IMG_0433.jpg",
+      "IMG_9925.JPG",
+      "text.jpg",
+      "IMG_0296.jpg",
+      "IMG_0352.jpg",
+      "IMG_0441.jpg",
+      "Megan 1.jpg",
+    ];
+
+    const items = []
+
+    for (const [index, value] of unmaskedFiles.entries()) {
+      items.push(<img key={index} src={`/unmasked/${value}`} height="800" alt={value} />)
+    }
+
+
+    return (
+      <Carousel autoPlay={3000} animationSpeed={1000} infinite={true}>
+        {items}
+      </Carousel>
+    );
+  }
 }
-
-export default App;
